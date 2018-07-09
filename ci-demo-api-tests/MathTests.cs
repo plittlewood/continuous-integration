2d67f5b415f4aa79bc80193647c07635dbc7a46b
plittlewood-rpt <plittlewood@reapit.com>
2018-07-09 12:53:56 +0100

    Added another test

--- ci-demo-api-tests/MathTests.cs
+++ ci-demo-api-tests/MathTests.cs
@@ -16,5 +16,16 @@ namespace ci_demo_api_tests
 
             Assert.Equal(5, output);
         }
+
+        [Fact]
+        public void When_Multiply_Result_Is6()
+        {
+            int numA = 2;
+            int numB = 3;
+
+            int output = MathHelper.Multiply(numA, numB);
+
+            Assert.Equal(6, output);
+        }
     }
 }
