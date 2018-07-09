78b67249a7b49df4a46fd7641024ef35e77c962c
plittlewood-rpt <plittlewood@reapit.com>
2018-07-09 11:31:45 +0100

    Removed silly endpoints

--- ci-demo-api/Controllers/DataController.cs
+++ ci-demo-api/Controllers/DataController.cs
@@ -22,26 +22,5 @@ namespace ci_demo_api.Controllers
         {
             return Ok($"The result of {numA} * {numB} is " + MathHelper.Multiply(numA, numB));
         }
-
-        // GET api/values
-        [HttpGet("newendpoint")]
-        public async Task<IActionResult> NewEndpoint()
-        {
-            return Ok("This is a new endpoint deployed using CI/CD methods");
-        }
-
-        // GET api/values
-        [HttpGet("anothernewendpoint")]
-        public async Task<IActionResult> AnotherNewEndpoint()
-        {
-            return Ok("This is another new endpoint deployed using CI/CD methods");
-        }
-
-        // GET api/values
-        [HttpGet("yetanothernewendpoint")]
-        public async Task<IActionResult> YetAnotherNewEndpoint()
-        {
-            return Ok("This is yet another new endpoint deployed using CI/CD methods");
-        }
     }
 }
