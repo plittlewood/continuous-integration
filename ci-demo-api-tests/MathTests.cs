1ba10059e00c29cfd707ae4833772d89fb2396e1
plittlewood-rpt <plittlewood@reapit.com>
2018-07-09 12:01:54 +0100

    Fixed test

--- ci-demo-api-tests/MathTests.cs
+++ ci-demo-api-tests/MathTests.cs
@@ -14,7 +14,7 @@ namespace ci_demo_api_tests
 
             int output = MathHelper.Multiply(numA, numB);
 
-            Assert.Equal(5, output);
+            Assert.Equal(4, output);
         }
     }
 }
