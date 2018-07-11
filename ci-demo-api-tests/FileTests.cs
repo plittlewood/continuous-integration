1159baec697e027d830d528bff00351c98d19a70
plittlewood-rpt <plittlewood@reapit.com>
2018-07-11 08:30:47 +0100

    Added FileHelper and associated test class

---
+++ ci-demo-api-tests/FileTests.cs
@@ -0,0 +1,21 @@
+﻿using ci_demo_api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ci_demo_api_tests
+{
+    public class FileTests
+    {
+        [Theory]
+        [InlineData("file_one.txt")]
+        [InlineData("file_two.txt")]
+        public void When_FileExists_ReturnContent(string path)
+        {
+            string output = FileHelper.GetFileContents(path);
+
+            Assert.NotEmpty(output);
+        }
+    }
+}
---
+++ ci-demo-api/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ci_demo_api
+{
+    public static class FileHelper
+    {
+        public static string GetFileContents(string path)
+        {
+            string fullPath = $@"/apps/apifiles/{path}";
+
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException($"Could not find file {fullPath}");
+            }
+
+            return File.ReadAllText(fullPath);
+        }
+    }
+}
